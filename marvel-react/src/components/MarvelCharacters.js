7df4d30445fdff6ad0101ade8e17cf0e1c1810af
Jobson Ribeiro <jobsonrisoa@gmail.com>
2021-12-08 22:28:54 -0300

    SearchItem implemented

--- marvel-react/src/components/MarvelCharacters.js
+++ marvel-react/src/components/MarvelCharacters.js
@@ -2,21 +2,27 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Card, Image } from 'antd';
 import Search from './Search';
+import MarvelLogo from '../assets/MarvelLogo.png';
 
 export default function MarvelCharacters() {
+    const [searchItem, setSearchItem] = useState('')
+    const searchTerm = (searchItem) => {
+        setSearchItem(searchItem)
+    }
     const [charactersData, setCharactersData] = useState([]);
     useEffect(() => {
-        axios.get(`https://gateway.marvel.com:443/v1/public/characters?apikey=e0da45f6c6d5ce9fb1437f04918d9d2c`)
+        axios.get(`https://gateway.marvel.com:443/v1/public/characters?name=${searchItem}&apikey=e0da45f6c6d5ce9fb1437f04918d9d2c`)
         .then((response) => {
             console.log(response.data.data.results)
             setCharactersData(response.data.data.results)    
         }).catch((error) => {
             console.error(error)
         })
-    }, [])
+    }, [searchItem])
     return (
         <div className="app-body"> 
-        <Search/> 
+        <img src={MarvelLogo} />
+        <Search searchTerm={searchTerm} /> 
             {charactersData.map((item) => {
                 return(
                     <Card title={item.name} className="card-body">
--- marvel-react/src/components/Search.js
+++ marvel-react/src/components/Search.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import Search from 'antd/lib/transfer/search';
+import { Input  } from 'antd';
+// eslint-disable-next-line
+import { AudioOutlined } from '@ant-design/icons';
 
+const { Search } = Input;
 
-export default function SearchComponent() {
+export default function SearchComponent({searchTerm}) {   
     return (
         <div>
-            <Search placeholder="Enter your character's name..." style={{ width: 400 }} />
+            <Search 
+            placeholder="Enter your character's name..." 
+            style={{ width: 500 }} 
+            onChange={(e) => searchTerm(e.target.value)}
+            />
+
         </div>
     )
 }
